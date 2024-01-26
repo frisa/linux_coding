98b53b1a2db205f1499b2c3fbc1edbe132ce6066
FRIDRICHOVSKY Jan <jan.fridrichovsky@leica-geosystems.com>
2024-01-26 16:43:52 +0100

    Simple signal handler added

--- signals.cpp
+++ signals.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <signal.h>
+#include <unistd.h>
+
+void sig_handler(int signo)
+{
+    std::cout << "recieved signal number: " << signo << std::endl;
+}
 
 int main()
 {
-    std::cout << "Linux codng example" << std::endl;
+    if (signal(SIGINT, sig_handler) == SIG_ERR)
+    {
+        std::cerr << "Error by registering handler" << std::endl;
+    }
+    while (true){
+        std::cout << "Process running\n";
+        sleep(1);
+    }
     return 0;
 }
